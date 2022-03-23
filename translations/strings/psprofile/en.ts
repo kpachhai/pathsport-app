3d5d16f4dcc4f9fc04ea3b9ca4ec17c97b3b3d60
Waqas Ahmed - Sharaf DG <waqas.m.ahmed.20@um.edu.mt>
2022-03-23 23:08:21 +0400

    Fixed translations on psprofile
    issue#19

--- translations/strings/psprofile/en.ts
+++ translations/strings/psprofile/en.ts
@@ -2,12 +2,33 @@ export const en = {
   psprofile: {
     'psprofile-profile': 'Profile',
     'enter-summary': 'Enter your professional sports journey',
-    'enter-fullname': '',
-    'enter-jerseyname': '',
-    'enter-height': '',
-    'enter-weight': '',
-    'enter-dob': '',
-    'enter-location': '',
+    'enter-fullname': 'Enter your full name',
+    'enter-jerseyname': 'Enter your jersey name',
+    'enter-height': 'Enter your height in cm',
+    'enter-weight': 'Enter your weight in kgs',
+    'enter-dob': 'Select your date of birth',
+    'enter-location': 'Select your location',
     'save-changes': 'Save Changes',
+
+    // add-stats.page
+    'enter-match-date': 'Select match date',
+    'enter-opponent-team': 'Enter opponent team',
+    'enter-team-score': 'Enter your team score',
+    'enter-opponent-score': 'Enter opponent score',
+    'enter-league': 'Enter your league',
+    'enter-goals': 'Enter your goals in the match',
+
+    // edit-professional-highlights.page
+    'enter-sport': 'Enter your sport name',
+    'enter-club': 'Enter your club name',
+    'enter-team': 'Enter your team name',
+    'enter-position': 'Enter your position e.g. Forward',
+
+    // edit-social-profiles.page
+    'enter-instagram': 'Enter your instagram handle',
+    'enter-facebook': 'Enter your facebook username',
+    'enter-twitter': 'Enter your twitter handle',
+    'enter-fifa': 'Enter your fifa username',
+    'enter-nba': 'Enter your nba username',
   },
 };
